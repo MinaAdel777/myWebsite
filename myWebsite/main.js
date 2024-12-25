b86262a02d6356799844592765f4d8f3d79d91a3
MinaAdel777 <106246832+MinaAdel777@users.noreply.github.com>
2024-12-25 21:41:55 +0200

    Delete myWebsite directory

--- myWebsite/main.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navbar = document.querySelector(".navbar");
-
-menuToggle.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
